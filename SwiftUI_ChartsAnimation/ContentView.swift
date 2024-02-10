6785a7a67aba981f49b5303349e166698841af7c
paku <boardguy1024@gmail.com>
2024-02-10 17:27:08 +0900

    Initial Commit

---
+++ SwiftUI_ChartsAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_ChartsAnimation
+//
+//  Created by paku on 2024/02/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_ChartsAnimation/SwiftUI_ChartsAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ChartsAnimationApp.swift
+//  SwiftUI_ChartsAnimation
+//
+//  Created by paku on 2024/02/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ChartsAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
